12f593cd3c5b536183fc39f74708475a3bb0c3be
VokazakDalv <Vokazak.Dalv@gmail.com>
2022-08-24 21:45:09 +0400

    fix: fix bug with pages display

--- src/app/core/app.ts
+++ src/app/core/app.ts
@@ -6,25 +6,27 @@ import { routing, defaultRoute } from './router';
 
 export class App {
   enableRouteChange = (): void => {
-    let main: Component;
+    let main: Component | HTMLElement;
+    let game: Component;
     window.onpopstate = () => {
       const currentRouteName = window.location.hash.slice(1);
       const currentRoute = routing.find((p) => p.name === currentRouteName);
       if (!document.body.children.length) {
         document.body.append(new Header().headerBlock, new Footer().footerBlock);
       } else {
-        main.node.remove();
+        (main as HTMLElement).remove();
       }
 
       if (currentRoute) {
-        main = currentRoute.component();
+        game = currentRoute.component();
+        main = game.node;
       } else if (defaultRoute) {
-        main.node = defaultRoute.component();
+        main = defaultRoute.component();
       }
-      document.body.lastElementChild?.before(main.node);
+      document.body.lastElementChild?.before(main as HTMLElement);
 
       if (currentRouteName === 'games/audio') {
-        const audioGame = main as AudioGame;
+        const audioGame = game as AudioGame;
         audioGame.levels.onLevel = (level) => {
           audioGame.levels.destroy();
           audioGame.gameLevel = level;
