be3a57da35d0599aad6617a81aeae8a0f71cc0d8
kasitsyn <kasitsyn@gmail.com>
2022-08-29 14:01:10 +0700

    feat: fetch words

--- src/app/components/auth/controls.ts
+++ src/app/components/auth/controls.ts
@@ -1,5 +1,5 @@
 import { LoginResponse } from '../../types/types';
-import { createUser, loginUser } from './model';
+import { createUser, loginUser } from '../../api/API';
 
 function showError(message: string): void {
   const error = document.createElement('div');
--- src/app/components/sprintGame/sprintGame.ts
+++ src/app/components/sprintGame/sprintGame.ts
@@ -1,8 +1,10 @@
+import { wordData } from '../../types/types';
 import { Component } from '../component';
 import { GameResult } from '../gameResult/gameResult';
 import { Levels } from '../levels/levels';
 import { Timer } from '../timer/timer';
 import './sprint.scss';
+import { getWords } from '../../api/API';
 
 export class SprintGame extends Component {
   sprintGameContainer = new Component(this.node, 'div', 'sprint-game__container', 'SPRINT').node;
@@ -15,13 +17,17 @@ export class SprintGame extends Component {
 
   timer: Timer;
 
+  wordsData: Promise<wordData[]>;
+
   constructor() {
     super(null, 'main', 'sprint-game');
 
     this.timer = new Timer(this.sprintGameContainer);
+
+    this.wordsData = getWords('0', '0');
   }
 
-  render() {
-    console.log(this.gameLevel);
+  async render() {
+    console.log(await this.wordsData);
   }
 }
